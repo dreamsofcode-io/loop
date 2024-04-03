0770723851c824dea2b88a0e531fd801b9fd0ed6
Elliott Minns <elliott.minns@pm.me>
2024-04-03 18:34:21 -0500

    package documentation

---
+++ doc.go
@@ -0,0 +1,14 @@
+// Package crayon provides a number of commonly used
+// methods for ranging over functions.
+//
+// Due to the experimental nature of the range-over-function feature
+// then this package will only work with go1.22 with the experimental
+// feature GOEXPERIMENT=rangefunc env var set
+//
+// ```go
+// GOEXPERIMENT=rangefunc go build
+// ```
+//
+// When this feature is no longer experiemental, then this
+// package will likely be much more useful.
+package crayon
